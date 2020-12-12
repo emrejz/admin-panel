fe4eaa2ab784845219f5ac0ebefab7ad24e67f50
emre kayacan <emre.kayacan@msn.com>
2020-12-12 14:29:39 +0300

    error handle and body-parser added

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const mongodb = require("./helpers/db/mongo");
 const customerProductRouter = require("./routes/customer/product");
 
+const port = 3001;
+
 //mongo connection
 mongodb();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 //todo auth middleware
 app.use("/api/costomer/product", customerProductRouter);
+
+//error handle
+app.use((err, req, res, next) => {
+  if (err) return res.status(400).json(err);
+  return next();
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
