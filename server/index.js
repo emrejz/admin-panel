fb39a8f2bbd20a45fe6c0b1cafb66f14bffae175
emre kayacan <emre.kayacan@msn.com>
2020-12-12 13:30:39 +0300

    dotenv imported and router name changed

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-const productCostomer = require("./routes/customer/product");
+require("dotenv").config();
+
+const customerProductRouter = require("./routes/customer/product");
 
 //todo auth middleware
-app.use("/api/costomer/product", productCostomer);
+app.use("/api/costomer/product", customerProductRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
