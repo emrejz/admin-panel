7cc014a1c5b6606581f8b818190df334734edecb
emre kayacan <emre.kayacan@msn.com>
2020-12-12 16:13:16 +0300

    swr global config wraped app

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { SWRConfig } from "swr";
+
 import App from "./components/app";
 import store from "./store";
 
 import "./index.scss";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <SWRConfig
+    value={{
+      refreshInterval: 3000,
+      fetcher: (resource, init) =>
+        fetch(resource, init).then((res) => res.json()),
+    }}
+  >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </SWRConfig>,
   document.getElementById("root")
 );
