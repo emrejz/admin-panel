3c3c416e04497f20e7ddad9aa89f6ea93768e155
emre kayacan <emre.kayacan@msn.com>
2020-12-18 00:54:50 +0300

    created hook helper for fetch operation

---
+++ client/src/helpers/useFetch.js
@@ -0,0 +1,42 @@
+import customNotification from "../components/customNotification";
+
+const useFetch = () => {
+  const message = (method) =>
+    method === "add" ? "ekleme" : method === "put" ? "güncelleme" : "silme";
+  const fetchOperation = async (method, path, body) => {
+    try {
+      const result = await fetch(
+        process.env.REACT_APP_CUSTOMER_PRODUCT_API + path,
+        {
+          method,
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+      const res = await result.json();
+
+      if (res.error) {
+        throw new Error(res.error.message);
+      } else {
+        customNotification({
+          title: "Başarılı!",
+          description: `Ürün ${message(method)} başarılı!`,
+        });
+        return true;
+      }
+    } catch (error) {
+      customNotification({
+        title: "Başarısız!",
+        description: `Ürün ${message(method)} başarısız oldu! Error: ${
+          error.message
+        }`,
+      });
+      throw new Error(error.message);
+    }
+  };
+
+  return {
+    fetchOperation,
+  };
+};
+export default useFetch;
