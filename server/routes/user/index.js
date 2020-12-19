f2c0dd80a15952ce8e479268369efabbb8123c35
emre kayacan <emre.kayacan@msn.com>
2020-12-19 16:42:10 +0300

    defined error codes and message each situation

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const userSchema = require("../../models/user");
 const { USER_ADMIN_ROLE } = require("../../constants");
-const { sign, verify } = require("../../helpers/jwt");
+const { sign } = require("../../helpers/jwt");
 
 router.post("/signup", async function (req, res, next) {
   try {
@@ -13,13 +13,12 @@ router.post("/signup", async function (req, res, next) {
       const hash = await bcrypt.hash(password, 10);
       const user = await new userSchema({ ...req.body, password: hash }).save();
       const token = await sign({ email, role });
-
       if (token) res.json({ token });
-      else next({ message: "Ups something went wrong!" });
-    } else next({ message: "Its not your business!" });
+      else next({ message: "Ups something went wrong!", code: 300 });
+    } else next({ message: "Its not your business!", code: 400 });
   } catch (error) {
     if (error.code == 11000) {
-      next({ message: "Email already exists!", code: 11000 });
+      next({ message: "Email already exists!", code: 100 });
     } else next(error);
   }
 });
@@ -28,13 +27,15 @@ router.post("/signin", async function (req, res, next) {
   try {
     const { password, email } = req.body;
     const user = await userSchema.findOne({ email });
+    if (!user) next({ message: "This email does not exist!", code: 101 });
+
     if (user) {
       const result = await bcrypt.compare(password, user.password);
       if (result) {
         const token = await sign({ email, role: user.role });
         if (token) res.json({ token });
-      } else next({ message: "Ups something went wrong!" });
-    } else next({ message: "Ups something went wrong!" });
+      } else next({ message: "Wrong password!", code: 200 });
+    } else next({ message: "Ups something went wrong!", code: 300 });
   } catch (error) {
     next(error);
   }
