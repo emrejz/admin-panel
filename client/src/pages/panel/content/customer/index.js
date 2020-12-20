f0e561313c2451eee3d30cb1a35a34f244751751
emre kayacan <emre.kayacan@msn.com>
2020-12-20 20:25:52 +0300

    customer content refacored

--- client/src/pages/panel/content/customer/index.js
+++ client/src/pages/panel/content/customer/index.js
@@ -1,96 +1,11 @@
-import { useState } from "react";
-import { Layout, List, Avatar, Typography, Spin } from "antd";
-import useSWR from "swr";
+import { useSelector } from "react-redux";
 
 //comps
-import ProductModal from "./modal";
-import CustomResult from "../../../../components/customResult";
-import CustomSkeleton from "../../../../components/customSkeleton";
-
-//helpers
-import useFetch from "../../../../helpers/useFetch";
-
-//scss
-import "./index.scss";
-
-const { Content } = Layout;
-const { Title, Text } = Typography;
+import CustomerProducts from "./product";
 
 export default () => {
-  const { data, error, mutate } = useSWR("/api/costomer/product");
-  const [deletingID, setDeletingID] = useState(null);
+  const { name } = useSelector((state) => state.selectedMenuItem);
 
-  const { fetchOperation } = useFetch();
-  const removeProduct = async (title, _id) => {
-    setDeletingID(_id);
-    try {
-      if (
-        window.confirm(title + " adlı ürünü silmek istediğinize emin misiniz?")
-      ) {
-        await fetchOperation("delete", "/api/costomer/product/delete", {
-          _id,
-        });
-        await mutate((data) => data.map((item) => item._id === _id));
-        setDeletingID(null);
-      }
-    } catch (error) {
-      setDeletingID(null);
-    }
-  };
-  return (
-    <Content className="productContentCont">
-      {error ? (
-        <CustomResult message={error.message} />
-      ) : data ? (
-        <>
-          <ProductModal />
-          <List
-            className="demo-loadmore-list"
-            itemLayout="horizontal"
-            dataSource={data}
-            renderItem={(item) => (
-              <List.Item
-                className={
-                  deletingID === item._id
-                    ? "productListItem deletingItem"
-                    : "productListItem"
-                }
-              >
-                <Avatar src={item.picture} className="avatar" />
-                <div className="infoBox">
-                  <Title level={5}>{item.title}</Title>
-                  <div>
-                    <Text strong>Fiyat: </Text>
-                    <Text>{item.price} TL</Text>
-                  </div>
-                  <div>
-                    <Text strong>Açıklama: </Text>
-                    <Text>{item.description}</Text>
-                  </div>
-                  <div className="buttons">
-                    {deletingID === item._id ? (
-                      <Spin size="small" />
-                    ) : (
-                      <>
-                        <ProductModal item={item} />
-                        {" | "}
-                        <Text
-                          type="danger"
-                          onClick={() => removeProduct(item.title, item._id)}
-                        >
-                          sil
-                        </Text>
-                      </>
-                    )}
-                  </div>
-                </div>
-              </List.Item>
-            )}
-          />
-        </>
-      ) : (
-        <CustomSkeleton n={3} />
-      )}
-    </Content>
-  );
+  if (name === "productList") return <CustomerProducts />;
+  else return <div></div>;
 };
---
+++ client/src/pages/panel/content/customer/product/index.js
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { Layout, List, Avatar, Typography, Spin } from "antd";
+import useSWR from "swr";
+
+//comps
+import ProductModal from "./modal";
+import CustomResult from "../../../../../components/customResult";
+import CustomSkeleton from "../../../../../components/customSkeleton";
+
+//helpers
+import useFetch from "../../../../../helpers/useFetch";
+
+//scss
+import "./index.scss";
+
+const { Content } = Layout;
+const { Title, Text } = Typography;
+
+export default () => {
+  const { data, error, mutate } = useSWR("/api/costomer/product");
+  const [deletingID, setDeletingID] = useState(null);
+
+  const { fetchOperation } = useFetch();
+  const removeProduct = async (title, _id) => {
+    setDeletingID(_id);
+    try {
+      if (
+        window.confirm(title + " adlı ürünü silmek istediğinize emin misiniz?")
+      ) {
+        await fetchOperation("delete", "/api/costomer/product/delete", {
+          _id,
+        });
+        await mutate((data) => data.map((item) => item._id === _id));
+        setDeletingID(null);
+      }
+    } catch (error) {
+      setDeletingID(null);
+    }
+  };
+  return (
+    <Content className="customerProductContentCont">
+      {error ? (
+        <CustomResult message={error.message} />
+      ) : data ? (
+        <>
+          <ProductModal />
+          <List
+            className="demo-loadmore-list"
+            itemLayout="horizontal"
+            dataSource={data}
+            renderItem={(item) => (
+              <List.Item
+                className={
+                  deletingID === item._id
+                    ? "customerProductListItem deletingItem"
+                    : "customerProductListItem"
+                }
+              >
+                <Avatar src={item.picture} className="avatar" />
+                <div className="infoBox">
+                  <Title level={5}>{item.title}</Title>
+                  <div>
+                    <Text strong>Fiyat: </Text>
+                    <Text>{item.price} TL</Text>
+                  </div>
+                  <div>
+                    <Text strong>Açıklama: </Text>
+                    <Text>{item.description}</Text>
+                  </div>
+                  <div className="buttons">
+                    {deletingID === item._id ? (
+                      <Spin size="small" />
+                    ) : (
+                      <>
+                        <ProductModal item={item} />
+                        {" | "}
+                        <Text
+                          type="danger"
+                          onClick={() => removeProduct(item.title, item._id)}
+                        >
+                          sil
+                        </Text>
+                      </>
+                    )}
+                  </div>
+                </div>
+              </List.Item>
+            )}
+          />
+        </>
+      ) : (
+        <CustomSkeleton n={3} />
+      )}
+    </Content>
+  );
+};
