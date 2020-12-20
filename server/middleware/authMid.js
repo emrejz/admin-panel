062c2370c99970278ab3b5c158004cbddca9af40
emre kayacan <emre.kayacan@msn.com>
2020-12-20 13:19:26 +0300

    created auth middleware

---
+++ server/middleware/authMid.js
@@ -0,0 +1,16 @@
+const { verify } = require("../helpers/jwt");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers["authorization"];
+    if (token && token !== "null") {
+      const user = verify(token);
+      req.user = user;
+      next();
+    } else {
+      next({ message: "Authentication failed!", code: 150 });
+    }
+  } catch (error) {
+    next({ message: "Authentication failed!", code: 150 });
+  }
+};
