5b10dea14b5199735b8998244a6a620364955ef5
emre kayacan <emre.kayacan@msn.com>
2020-12-20 17:10:27 +0300

    moved customer menu list to constants

--- client/src/constants/errors/index.js
+++ client/src/constants/errors/index.js
@@ -1,2 +1,29 @@
+import {
+  ShoppingCartOutlined,
+  NotificationOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
+
 export const emailErrorCodes = [100, 101];
 export const passwordErrorCodes = [200];
+
+export const customerMenuList = [
+  {
+    icon: <ShoppingCartOutlined />,
+    title: "Ürün",
+    items: [{ payload: "productList", title: "Listele" }],
+  },
+  {
+    icon: <NotificationOutlined />,
+    title: "Sipariş",
+    items: [
+      { payload: "orderNew", title: "Bekleyen" },
+      { payload: "orderList", title: "Listele" },
+    ],
+  },
+  {
+    icon: <ProfileOutlined />,
+    title: "Stok",
+    items: [{ payload: "stockList", title: "Listele" }],
+  },
+];
