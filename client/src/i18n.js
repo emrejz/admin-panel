560710dcf5b43242890833299e1e35422d6231c3
emre kayacan <emre.kayacan@msn.com>
2020-12-21 21:16:37 +0300

    created i18n config file

---
+++ client/src/i18n.js
@@ -0,0 +1,35 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+//translations
+import translationEN from "./locales/en/translation.json";
+import translationTR from "./locales/tr/translation.json";
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  tr: {
+    translation: translationTR,
+  },
+};
+const detection = {
+  order: ["querystring", "navigator"],
+  lookupQuerystring: "lng",
+};
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    detection,
+    fallbackLng: "tr",
+    keySeparator: false,
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
