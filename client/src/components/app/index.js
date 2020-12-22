d9c65cc1fff0889dac27920668bd27e4dad73023
emre kayacan <emre.kayacan@msn.com>
2020-12-22 18:30:41 +0300

    languageSwitch comp imported to app

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -1,10 +1,12 @@
 import Root from "../../router/index";
+import LanguageSwitch from "../languageSwitch";
 
 import "./index.scss";
 
 function App() {
   return (
     <div className="App">
+      <LanguageSwitch />
       <Root />
     </div>
   );
