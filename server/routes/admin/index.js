505cf677f472ed42bc84d11a76643e7bdc759bd6
emre kayacan <emre.kayacan@msn.com>
2020-12-24 12:46:46 +0300

    delete user route ok

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 //models
 const userSchema = require("../../models/user");
+const productSchema = require("../../models/customer/product");
 
 router.get("/user/list", async function (req, res, next) {
   try {
@@ -12,6 +13,20 @@ router.get("/user/list", async function (req, res, next) {
       .sort({ email: 1 });
     if (users) {
       res.json({ users });
+    } else next({ message: "Ups something went wrong!", code: 300 });
+  } catch (error) {
+    next(error);
+  }
+});
+router.delete("/user/delete", async function (req, res, next) {
+  const { _id, deleteProducts } = req.body;
+  try {
+    const user = await userSchema.findByIdAndDelete(req.body._id);
+    if (deleteProducts) {
+      const products = await productSchema.remove({ customerID: _id });
+    }
+    if (user) {
+      res.json({ user });
     } else next({ message: "Ups something went wrong!", code: 300 });
   } catch (error) {
     next(error);
