41f8db8dc6453ccd61c31e008e7850fdbbe48ded
emre kayacan <emre.kayacan@msn.com>
2020-12-24 15:19:49 +0300

    created edit user route

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -13,23 +13,39 @@ router.get("/user/list", async function (req, res, next) {
       .sort({ email: 1 });
     if (users) {
       res.json({ users });
-    } else next({ message: "Ups something went wrong!", code: 300 });
+    } else {
+      return next({ message: "Ups something went wrong!", code: 300 });
+    }
   } catch (error) {
-    next(error);
+    return next(error);
   }
 });
 router.delete("/user/delete", async function (req, res, next) {
   const { _id, deleteProducts } = req.body;
   try {
-    const user = await userSchema.findByIdAndDelete(req.body._id);
+    const user = await userSchema.findByIdAndDelete(_id);
     if (deleteProducts) {
       const products = await productSchema.remove({ customerID: _id });
     }
     if (user) {
       res.json({ user });
+    } else {
+      return next({ message: "Ups something went wrong!", code: 300 });
+    }
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.put("/user/edit", async function (req, res, next) {
+  const { _id, role, email } = req.body;
+  try {
+    const user = await userSchema.findByIdAndUpdate(_id, { role, email });
+    if (user) {
+      res.json({ user });
     } else next({ message: "Ups something went wrong!", code: 300 });
   } catch (error) {
-    next(error);
+    return next(error);
   }
 });
 
