ef05a4afb57ce2fbdfff87a546b7f859abc11f69
emre kayacan <emre.kayacan@msn.com>
2020-12-24 15:17:43 +0300

    file name changed, user info change control added

--- server/middleware/userStatusMid.js
+++ server/middleware/userStatusMid.js
@@ -3,12 +3,20 @@ const userSchema = require("../models/user");
 module.exports = async (req, res, next) => {
   console.log(req.user);
   try {
-    const { _id } = req.user;
+    const { _id, email, role } = req.user;
     const result = await userSchema.findById(_id);
     if (!result && result !== "null") {
       return next({
-        message: "Authentication failed, please contact to admin!",
+        message:
+          "Authentication failed. Your account may have been removed please contact to admin!",
         code: 152,
+      });
+    }
+    if (result.email !== email || result.role !== role) {
+      return next({
+        message:
+          "Authentication failed. Your account may have been changed please contact to admin!",
+        code: 153,
       });
     } else {
       return next();
